6b6c2327c0ba1d634e026050417026b81ff53bfa
Brad Davies <varbrad@users.noreply.github.com>
2017-05-19 19:39:18 +0100

    Further tests for Collection

--- test/collection.js
+++ test/collection.js
@@ -28,6 +28,11 @@ describe('MinDB.Collection', () => {
     expect(col.hello, 'not to be empty')
   })
 
+  it('Should mean that proxy access === get()', () => {
+    col.insert({ _id: 'bonjour', name: 'foobar' })
+    expect(col.bonjour, 'to equal', col.get('bonjour'))
+  })
+
   describe('#empty()', () => {
     it('Should empty the collection', () => {
       col.insert({ _id: 'test', data: 123 })
@@ -84,21 +89,99 @@ describe('MinDB.Collection', () => {
       }
       expect(err, 'to be defined')
     })
+
+    it('Should return "undefined" for a non-existant id', () => {
+      expect(col.get('not-found'), 'to be undefined')
+    })
   })
 
   describe('#insert()', () => {
+    it('Should insert a single document', () => {
+      expect(col.list(), 'to be empty')
+      col.insert({ _id: 'wow', much: 'memes' })
+      expect(col.list(), 'to have length', 1)
+      expect(col.wow.much, 'to equal', 'memes')
+    })
 
+    it('Should throw error on duplicate id', () => {
+      col.insert({ _id: 'hi', bob: true })
+      let err
+      try {
+        col.insert({ _id: 'hi', error: 'hopefully!' })
+      } catch (e) {
+        err = e
+      }
+      expect(err, 'to be defined')
+    })
+
+    it('Should throw error on document with no id', () => {
+      let err
+      try {
+        col.insert({ noId: true })
+      } catch (e) {
+        err = e
+      }
+      expect(err, 'to be defined')
+    })
+
+    it('Should throw error on non-string id', () => {
+      let err
+      try {
+        col.insert({ _id: ['bob'], haha: true, o: 50.5 })
+      } catch (e) {
+        err = e
+      }
+      expect(err, 'to be defined')
+    })
+
+    it('Should ensure document doesnt use reserved word as id', () => {
+      let err
+      try {
+        col.insert({ _id: 'upsert', data: 'fail!' })
+      } catch (e) {
+        err = e
+      }
+      expect(err, 'to be defined')
+    })
+
+    it('Should allow update if overwrite is set', () => {
+      col.insert({ _id: 'data', value: 50 })
+      col.insert({ _id: 'data', value: 100 }, true)
+      expect(col.data.value, 'to equal', 100)
+    })
   })
 
   describe('#list()', () => {
+    it('Should show empty list', () => {
+      expect(col.list(), 'to be empty')
+    })
 
+    it('Should have one named entry', () => {
+      col.insert({ _id: 'test', value: 9 })
+      expect(col.list(), 'to have length', 1)
+      expect(col.list(), 'to contain', 'test')
+    })
   })
 
   describe('#upsert()', () => {
-
+    it('Should use insert(..., true) implementation', () => {
+      col.insert({ _id: 'fred', value: 95.2 })
+      col.insert({ _id: 'fred', cocoa: 18 }, true)
+      expect(col.fred.value, 'to be undefined')
+      expect(col.fred.cocoa, 'to equal', 18)
+    })
   })
 
   describe('#values()', () => {
+    it('Should show empty values', () => {
+      expect(col.values(), 'to be empty')
+    })
 
+    it('Should have one entry', () => {
+      col.insert({ _id: 'bob', kerbal: true })
+      expect(col.values(), 'to have length', 1)
+      expect(col.values()[0], 'to have key', 'kerbal')
+      expect(col.values()[0], 'to equal', col.bob)
+    })
   })
 })
