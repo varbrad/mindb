803725fba940f78b398f109438004ddf5b3fdfd3
Brad Davies <varbrad@users.noreply.github.com>
2017-05-19 16:37:47 +0100

    Update configs to generate .d.ts files

--- config/rollup.esm.js
+++ config/rollup.esm.js
@@ -1,7 +1,7 @@
 const babel = require('rollup-plugin-babel')
 
 export default {
-  entry: 'dist/es2017/mindb.js',
+  entry: 'dist/es2017/main.js',
   format: 'es',
   dest: 'dist/mindb.esm.js',
   external: ['clone'],
